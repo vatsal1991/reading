f025fd9db1f9af50ecccef65096443c1d708cffc
Rohit Ghumare <ghumare64@gmail.com>
2024-11-09 12:27:41 +0000

    gh pages commit

---
+++ sidebars.js
@@ -0,0 +1,16 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'DevOps Questions',
+      items: [
+        'core-concepts/introduction',
+        'containerization/docker',
+        'containerization/kubernetes',
+        'ci-cd/basics',
+        'infrastructure/terraform',
+        'monitoring/overview'
+      ],
+    },
+  ],
+}; 
